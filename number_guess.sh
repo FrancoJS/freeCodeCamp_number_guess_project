ee2db60b07041c6033c527f5a56cf9179ae02419
FrancoJS <jean.jara.fs@gmail.com>
2024-07-29 00:11:20 +0000

    feat: Agregue funcionalidad para agregar usuario

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+NUMBER=$((( RANDOM % 1000 ) + 1 ))
+
+MAIN(){
+  echo "Enter your username:"
+  read USERNAME
+  USER_ID=$($PSQL "select user_id from users where username = '$USERNAME'")
+  if [[ -z $USER_ID  ]]
+  then
+    INSERT_USER_RESULT=$($PSQL "insert into users(username,games_played) values('$USERNAME', 0)")
+    if [[ $INSERT_USER_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Welcome, $USERNAME! It looks like this is your first time here."
+    fi
+  fi
+}
+
+MAIN
