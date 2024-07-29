6bcff19c2c8031e8e1ea155ec58cfdd88a553576
FrancoJS <jean.jara.fs@gmail.com>
2024-07-29 01:09:45 +0000

    feat: agregue funcionalidades de best_game y games_played

--- number_guess.sh
+++ number_guess.sh
@@ -22,7 +22,8 @@ MAIN(){
   fi
 
   echo "Guess the secret number between 1 and 1000:"
-  GUESS_NUMBER $1 
+  GUESS_NUMBER $1 $USER_ID
+  UPDATE_GAMES_PLAYED=$($PSQL "update users set games_played = games_played + 1 where user_id = $USER_ID")
   
 }
 
@@ -37,21 +38,27 @@ GUESS_NUMBER(){
   NUMBER_GUESSES=0
   while [ $USER_NUMBER -ne $1 ]
   do
-    if [[ $USER_NUMBER > $1 ]]
+    if [[ $USER_NUMBER -gt $1 ]]
     then
+      NUMBER_GUESSES=$((NUMBER_GUESSES + 1))
       echo "It's lower than that, guess again:"
-      NUMBER_GUESSES+=1
       read USER_NUMBER
     fi
-    if [[ $USER_NUMBER < $1 ]]
+    if [[ $USER_NUMBER -lt $1 ]]
     then
+      NUMBER_GUESSES=$((NUMBER_GUESSES + 1))
       echo "It's higher than that, guess again:"
-      NUMBER_GUESSES+=1
       read USER_NUMBER
     fi
     if [[ $USER_NUMBER == $1 ]]
     then
+      NUMBER_GUESSES=$((NUMBER_GUESSES + 1))
       echo "You guessed it in $NUMBER_GUESSES tries. The secret number was $1. Nice job!"
+      BEST_GAME=$($PSQL "select best_game from users where user_id = $2")
+      if [[ -z $BEST_GAME || $NUMBER_GUESSES -lt $BEST_GAME ]]
+      then
+        UPDATE_BEST_GAME=$($PSQL "update users set best_game = $NUMBER_GUESSES where user_id = $2")
+      fi
     fi
   done
 }
